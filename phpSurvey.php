8a1540d24e5663aa06c15131ea5a159c59e0aea0
jcomish <jomish2323@gmail.com>
2015-10-03 16:52:24 -0500

    assignment 2

--- phpSurvey.php
+++ phpSurvey.php
@@ -21,73 +21,86 @@
     $Q3 = $_POST["animal"];
     $Q4 = $_POST["cake"];
 
+	$_fp = fopen("results.txt", "r");
+	fscanf($_fp, "%d\n", $count);
+	$numbers = explode(" ", trim(fgets($_fp)));
+	foreach ($numbers as &$number)
+	{
+	    $number = intval($number);
+	}
+
     //Question 1
     if ($Q1 == "Burritos")
     {
     	echo "Burritios!"
+    	++$numbers[0];
     }
     elseif($Q1 == "Bagels")
     {
-
+    	++$numbers[1];
     }
     elseif($Q1 == "Steak")
     {
-
+    	++$numbers[2];
     }
     elseif($Q1 == "Pizza")
     {
-
+    	++$numbers[3];
     }
 
     //Question 2
     if ($Q2 == "Red")
     {
-
+    	++$numbers[4];
     }
     elseif($Q2 == "Blue")
     {
-
+    	++$numbers[5];
     }
     elseif($Q2 == "Green")
     {
-
+    	++$numbers[6];
     }
     elseif($Q2 == "Moave")
     {
-
+    	++$numbers[7];
     }
 
     //Question 3
     if ($Q3 == "Bears")
     {
-
+    	++$numbers[8];
     }
     elseif($Q3 == "Cats")
     {
-
+    	++$numbers[9];
     }
     elseif($Q3 == "Bear Cats")
     {
-
+    	++$numbers[10];
     }
     elseif($Q3 == "Snakes")
     {
-
+    	++$numbers[11];
     }
 
     //Question 4
     if ($Q4 == "yes")
     {
-
+    	++$numbers[12];
     }
     elseif($Q4 == "no")
     {
-
+    	++$numbers[13];
     }
 
-    ?>
 
-	<!--if ($_POST["item1"] == 'on') -->
+	$_fp = fopen('results.txt', 'w');
+
+	foreach ($numbers as $number) {
+	    fputs($_fp, $number);
+	}
+    ?>
 
 
 </body>
