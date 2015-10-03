0b15fcd02713890f7578542af30ed9dab7e116e4
jcomish <jomish2323@gmail.com>
2015-10-03 15:15:30 -0500

    assignment 2

--- teamReadiness1.php
+++ teamReadiness1.php
@@ -4,7 +4,13 @@
 Name: <?php echo $_POST["name"]; ?><br/>
 Email Address: <?php echo $_POST["email"]; ?><br/>
 Major: <?php echo $_POST["major"]; ?><br/>
-Place Visited: <?php echo $_POST["item1"]; ?> <?php echo $_POST["item2"]; ?> <?php echo $_POST["item3"]; ?> <?php echo $_POST["item4"]; ?> <?php echo $_POST["item5"]; ?> <?php echo $_POST["item6"]; ?> <?php echo $_POST["item7"]; ?>
+Place Visited: <?php echo $_POST["item1"]; ?> 
+			   <?php echo $_POST["item2"]; ?> 
+			   <?php echo $_POST["item3"]; ?> 
+			   <?php echo $_POST["item4"]; ?> 
+			   <?php echo $_POST["item5"]; ?> 
+			   <?php echo $_POST["item6"]; ?> 
+			   <?php echo $_POST["item7"]; ?>
 Comments: <?php echo $_POST["comments"]; ?><br/>
 
 </body>
