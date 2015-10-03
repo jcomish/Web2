896327e9d5dcdda2f09ee80b2dcbf8b7343550ed
jcomish <jomish2323@gmail.com>
2015-10-03 15:26:07 -0500

    assignment 2

--- teamReadiness1.php
+++ teamReadiness1.php
@@ -4,17 +4,48 @@
 Name: <?php echo $_POST["name"]; ?><br/>
 Email Address: <?php echo $_POST["email"]; ?><br/>
 Major: <?php echo $_POST["major"]; ?><br/>
-Place Visited: <?php if ($_POST["item1"] == 'on') 
-					{
-						echo "North America, ";
-					}?> 
-			   <?php echo $_POST["item2"]; ?> 
-			   <?php echo $_POST["item3"]; ?> 
-			   <?php echo $_POST["item4"]; ?> 
-			   <?php echo $_POST["item5"]; ?> 
-			   <?php echo $_POST["item6"]; ?> 
-			   <?php echo $_POST["item7"]; ?><br/>
-Comments: <?php echo $_POST["comments"]; ?><br/>
+Place Visited: 
+<?php 
+	if ($_POST["item1"] == 'on') 
+	{
+		echo "North America, ";
+	}
+
+	if ($_POST["item2"] == 'on')
+	{
+		echo "South America, ";
+	}
+
+	if ($_POST["item3"] == 'on')
+	{
+		echo "Europe, ";
+	}
+
+	if ($_POST["item4"] == 'on')
+	{
+		echo "Asia, ";
+	}
+
+	if ($_POST["item5"] == 'on')
+	{
+		echo "Austrailia, ";
+	}
+
+	if ($_POST["item6"] == 'on')
+	{
+		echo "Africa, ";
+	}
+
+	if ($_POST["item7"] == 'on')
+	{
+		echo "Antarctica.";
+	}
+
+?><br/>
+
+Comments: <br/>
+<?php echo $_POST["comments"]; ?>
+<br/>
 
 </body>
 </html>
