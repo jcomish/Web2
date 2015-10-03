f2f0364459a56ca4ec96c5c63228ede38e154b13
jcomish <jomish2323@gmail.com>
2015-10-03 15:29:28 -0500

    assignment 2

--- teamReadiness1.php
+++ teamReadiness1.php
@@ -4,41 +4,41 @@
 Name: <?php echo $_POST["name"]; ?><br/>
 Email Address: <?php echo $_POST["email"]; ?><br/>
 Major: <?php echo $_POST["major"]; ?><br/>
-Place Visited: 
+Place Visited: <br/>
 <?php 
 	if ($_POST["item1"] == 'on') 
 	{
-		echo "North America, ";
+		echo "North America\n";
 	}
 
 	if ($_POST["item2"] == 'on')
 	{
-		echo "South America, ";
+		echo "South America\n";
 	}
 
 	if ($_POST["item3"] == 'on')
 	{
-		echo "Europe, ";
+		echo "Europe\n";
 	}
 
 	if ($_POST["item4"] == 'on')
 	{
-		echo "Asia, ";
+		echo "Asia\n";
 	}
 
 	if ($_POST["item5"] == 'on')
 	{
-		echo "Austrailia, ";
+		echo "Austrailia\n";
 	}
 
 	if ($_POST["item6"] == 'on')
 	{
-		echo "Africa, ";
+		echo "Africa\n";
 	}
 
 	if ($_POST["item7"] == 'on')
 	{
-		echo "Antarctica.";
+		echo "Antarctica\n";
 	}
 
 ?><br/>
