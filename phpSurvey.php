fb00b2d31c73784b1d6b8d53ca75499ebc3478d8
jcomish <jomish2323@gmail.com>
2015-10-03 16:23:46 -0500

    assignment 2

--- phpSurvey.php
+++ phpSurvey.php
@@ -1,51 +1,94 @@
-<html>
-<body>
+<html lang = "en">
+  <head>
+    <title>Joshua Comish</title>
+    <link rel="stylesheet" type="text/css" href="Jomish.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8"/>
+  </head>
 
+  <header>
+    </br>
+    <h4><a href="index.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Home</a> </h4>
+    <h4><a href="assign032.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Projects</a> </h4>
+    <h4><a href="assignments.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Assignments</a></h4>
+    </br>
+    </br>
+  </header>
+
+  <body>
+    <?php
+    $Q1 = $_POST["food"];
+    $Q2 = $_POST["color"];
+    $Q3 = $_POST["animal"];
+    $Q4 = $_POST["cake"];
+
+    //Question 1
+    if ($Q1 == "Burritos")
+    {
+    	echo "Burritios!"
+    }
+    elseif($Q1 == "Bagels")
+    {
+
+    }
+    elseif($Q1 == "Steak")
+    {
+
+    }
+    elseif($Q1 == "Pizza")
+    {
+
+    }
+
+    //Question 2
+    if ($Q2 == "Red")
+    {
+
+    }
+    elseif($Q2 == "Blue")
+    {
+
+    }
+    elseif($Q2 == "Green")
+    {
+
+    }
+    elseif($Q2 == "Moave")
+    {
+
+    }
+
+    //Question 3
+    if ($Q3 == "Bears")
+    {
+
+    }
+    elseif($Q3 == "Cats")
+    {
+
+    }
+    elseif($Q3 == "Bear Cats")
+    {
+
+    }
+    elseif($Q3 == "Snakes")
+    {
+
+    }
+
+    //Question 4
+    if ($Q4 == "yes")
+    {
+
+    }
+    elseif($Q4 == "no")
+    {
+
+    }
+
+    ?>
 Name: <?php echo $_POST["name"]; ?><br/>
-Email Address: <?php echo $_POST["email"]; ?><br/>
-Major: <?php echo $_POST["major"]; ?><br/>
-Place Visited: <br/>
-<?php 
-	if ($_POST["item1"] == 'on') 
-	{
-		echo "North America<br/>";
-	}
+	<!--if ($_POST["item1"] == 'on') -->
 
-	if ($_POST["item2"] == 'on')
-	{
-		echo "South America<br/>";
-	}
-
-	if ($_POST["item3"] == 'on')
-	{
-		echo "Europe<br/>";
-	}
-
-	if ($_POST["item4"] == 'on')
-	{
-		echo "Asia<br/>";
-	}
-
-	if ($_POST["item5"] == 'on')
-	{
-		echo "Austrailia<br/>";
-	}
-
-	if ($_POST["item6"] == 'on')
-	{
-		echo "Africa<br/>";
-	}
-
-	if ($_POST["item7"] == 'on')
-	{
-		echo "Antarctica<br/>";
-	}
-
-?><br/>
-
-Comments: <br/>
-<?php echo $_POST["comments"]; ?>
-<br/>
 
 </body>
 </html>
