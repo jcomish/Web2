722b37310f7e53fd2c285b2810fb371b4acdd321
jcomish <jomish2323@gmail.com>
2015-10-15 21:38:35 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -1,50 +1,84 @@
-<!DOCTYPE html>
-<html lang = "en">
 
-<head>
-    <title>Joshua Comish</title>
-    <link rel="stylesheet" type="text/css" href="Jomish.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8"/>
+<?php
+$con=mysql_connect("localhost","Username","Password") or die("Failed to connect with database!!!!");
+mysql_select_db("Database Name", $con); 
+// The Chart table contains two fields: weekly_task and percentage
+// This example will display a pie chart. If you need other charts such as a Bar chart, you will need to modify the code a little to make it work with bar chart and other charts
+$sth = mysql_query("SELECT * FROM chart");
+
+/*
+---------------------------
+example data: Table (Chart)
+--------------------------
+weekly_task     percentage
+Sleep           30
+Watching Movie  40
+work            44
+*/
+
+$rows = array();
+//flag is not needed
+$flag = true;
+$table = array();
+$table['cols'] = array(
+
+    // Labels for your chart, these represent the column titles
+    // Note that one column is in "string" format and another one is in "number" format as pie chart only required "numbers" for calculating percentage and string will be used for column title
+    array('label' => 'Weekly Task', 'type' => 'string'),
+    array('label' => 'Percentage', 'type' => 'number')
+
+);
+
+$rows = array();
+while($r = mysql_fetch_assoc($sth)) {
+    $temp = array();
+    // the following line will be used to slice the Pie chart
+    $temp[] = array('v' => (string) $r['Weekly_task']); 
+
+    // Values of each slice
+    $temp[] = array('v' => (int) $r['percentage']); 
+    $rows[] = array('c' => $temp);
+}
+
+$table['rows'] = $rows;
+$jsonTable = json_encode($table);
+//echo $jsonTable;
+?>
+
+<html>
+  <head>
+    <!--Load the Ajax API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script type="text/javascript">
+
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      // Create our data table out of JSON data loaded from server.
+      var data = new google.visualization.DataTable(<?=$jsonTable?>);
+      var options = {
+           title: 'My Weekly Plan',
+          is3D: 'true',
+          width: 800,
+          height: 600
+        };
+      // Instantiate and draw our chart, passing in some options.
+      // Do not forget to check your div ID
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
+    </script>
   </head>
 
-  <header>
-    </br>
-    <h4><a href="index.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Home</a> </h4>
-    <h4><a href="assign032.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Projects</a> </h4>
-    <h4><a href="assignments.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Assignments</a></h4>
-    </br>
-    </br>
-  </header>
   <body>
-    <?php
-
-      define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-      define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
-      define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-      define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-      define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
-      try
-      {
-        $dsn = 'mysql:dbname=scriptures;host='.DB_HOST.';port='.DB_PORT;
-        $db = new PDO($dsn, DB_USER, DB_PASS);
-      }
-      catch (PDOException $ex)
-      {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-      }
-
-      $statement = $db->query('USE scriptures');
-      $statement = $db->query('SELECT * FROM scriptures');
-      while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-      {
-         echo '<p2><b>' . $row['book'] . '</b> <b>';                                                                                                                                   
-         echo $row['chapter'] . '</b>:<b>';                                                                                                                                            
-         echo $row['verse'] . '</b> - "';                                                                                                                                              
-         echo $row['content'] . '"</br></br></p2>';
-      }
-
-
-    ?>
+    <!--this is the div that will hold the pie chart-->
+    <div id="chart_div"></div>
   </body>
 </html>
+  
