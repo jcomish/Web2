81549fd701df54f1e17313ac5ffae5d4e553e9d2
jcomish <jomish2323@gmail.com>
2015-10-17 16:53:06 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -137,6 +137,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 
       var chart = new google.charts.Line(document.getElementById('linechart_material2'));
       chart.draw(data, options);
+    //test
     
 
   </script>
