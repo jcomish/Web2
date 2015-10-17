6b2d36f82f9106149ae2a1d696ca9f16d40c5819
jcomish <jomish2323@gmail.com>
2015-10-17 00:54:24 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <title>Joshua Comish</title>
+    <link rel="stylesheet" type="text/css" href="Jomish.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8"/>
+  </head>
+
+  <header>
+    </br>
+    <h4><a href="index.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Home</a> </h4>
+    <h4><a href="assign032.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Projects</a> </h4>
+    <h4><a href="assignments.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Assignments</a></h4>
+    </br>
+    </br>
+  </header>
 
 <?php
  define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
@@ -17,14 +33,14 @@
       }
 
 //Get the data
-$statement = $db->query("USE project");
+/*$statement = $db->query("USE project");
 $statement = echo $sth->query("SELECT * FROM rel")
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     { 
        echo '<p2><b>' . $row['rel_id'] . '</b> <b>';
        echo $row['name'] . '</b>:<b>';
-       echo $row['due_date'] . '</b> </br></br></p2>';
-    }
+       echo $row['due_date'] . '</b></br></br></p2>';
+    }*/
 ?>
 
 
