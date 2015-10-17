a508c43b46d43830673adacbc9e44ad1a06c1883
jcomish <jomish2323@gmail.com>
2015-10-17 11:57:56 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -64,33 +64,12 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 
     function drawChart() {
 
-      var data1 = new google.visualization.DataTable();
-      data1.addColumn('date', 'Time (In Hours)');
-      data1.addColumn('number', 'Due Date');
-      data1.addColumn('number', 'Progress');
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'Time (In Hours)');
+      data.addColumn('number', 'Due Date');
+      data.addColumn('number', 'Progress');
 
-      data1.addRows([
-        [new Date(2015, 10, 16),  56, 55],
-        [new Date(2015, 10, 17),  52, 53],
-        [new Date(2015, 10, 18),  48, 51],
-        [new Date(2015, 10, 19),  44, 49],
-        [new Date(2015, 10, 20),  40, 42],
-        [new Date(2015, 10, 21),  36, 36],
-        [new Date(2015, 10, 22),  32, 31],
-        [new Date(2015, 10, 23),  28, 25],
-        [new Date(2015, 10, 24),  24, 20],
-        [new Date(2015, 10, 25), 20, 15],
-        [new Date(2015, 10, 26), 16,  11],
-        [new Date(2015, 10, 27), 12,  6],
-        [new Date(2015, 10, 28), 8,  2],
-        [new Date(2015, 10, 29), 0,  0]
-      ]);
-      var data2 = new google.visualization.DataTable();
-      data2.addColumn('date', 'Time (In Hours)');
-      data2.addColumn('number', 'Due Date');
-      data2.addColumn('number', 'Progress');
-
-      data2.addRows([
+      data.addRows([
         [new Date(2015, 10, 16),  56, 55],
         [new Date(2015, 10, 17),  52, 53],
         [new Date(2015, 10, 18),  48, 51],
@@ -107,7 +86,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         [new Date(2015, 10, 29), 0,  0]
       ]);
 
-      var options1 = {
+      var options = {
         chart: {
           title: 'Rocket Boots',
           subtitle: 'Time remaining in hours'
@@ -116,23 +95,49 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         height: 500
       };
 
-      var options2 = {
+      var chart = new google.charts.Line(document.getElementById('linechart_material1'));
+
+      chart.draw(data, options);
+    }
+
+    function drawChart2() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'Time (In Hours)');
+      data.addColumn('number', 'Due Date');
+      data.addColumn('number', 'Progress');
+
+      data.addRows([
+        [new Date(2015, 10, 16),  56, 55],
+        [new Date(2015, 10, 17),  52, 53],
+        [new Date(2015, 10, 18),  48, 51],
+        [new Date(2015, 10, 19),  44, 49],
+        [new Date(2015, 10, 20),  40, 42],
+        [new Date(2015, 10, 21),  36, 36],
+        [new Date(2015, 10, 22),  32, 31],
+        [new Date(2015, 10, 23),  28, 25],
+        [new Date(2015, 10, 24),  24, 20],
+        [new Date(2015, 10, 25), 20, 15],
+        [new Date(2015, 10, 26), 16,  11],
+        [new Date(2015, 10, 27), 12,  6],
+        [new Date(2015, 10, 28), 8,  2],
+        [new Date(2015, 10, 29), 0,  0]
+      ]);
+
+      var options = {
         chart: {
-          title: 'Power Generator',
+          title: 'Rocket Boots',
           subtitle: 'Time remaining in hours'
         },
         width: 900,
         height: 500
       };
 
-      var chart = new google.charts.Line(document.getElementById('chart_div1'));
-      //var chart2 = new google.charts.Line(document.getElementById('linechart_material2'));
-      //var chart1 = new google.visualization.LineChart(document.getElementById('chart_div1'));
-      //      chart1.draw(data1, options1);
-      //var chart2 = new google.visualization.LineChart(document.getElementById('chart_div2'));
-      //      chart2.draw(data2, options2);
+      var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
       chart.draw(data, options);
     }
+
   </script>
 
 
@@ -155,8 +160,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   </br>
 
 <!--this is the div that will hold the pie chart-->
-<div id="chart_div1"></div>
-
+<div id="linechart_material"></div>
 </body>
 
 
