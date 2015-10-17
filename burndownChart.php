3f5ba1920802ed270d28f00043a576f81b46e623
jcomish <jomish2323@gmail.com>
2015-10-17 08:07:01 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -58,26 +58,25 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     function drawChart() {
 
       var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Day');
-      data.addColumn('number', 'Guardians of the Galaxy');
-      data.addColumn('number', 'The Avengers');
-      data.addColumn('number', 'Transformers: Age of Extinction');
+      data.addColumn('number', 'Time (In Hours)');
+      data.addColumn('number', 'Due Date');
+      data.addColumn('number', 'Progress');
 
       data.addRows([
-        [1,  37.8, 80.8, 41.8],
-        [2,  30.9, 69.5, 32.4],
-        [3,  25.4,   57, 25.7],
-        [4,  11.7, 18.8, 10.5],
-        [5,  11.9, 17.6, 10.4],
-        [6,   8.8, 13.6,  7.7],
-        [7,   7.6, 12.3,  9.6],
-        [8,  12.3, 29.2, 10.6],
-        [9,  16.9, 42.9, 14.8],
-        [10, 12.8, 30.9, 11.6],
-        [11,  5.3,  7.9,  4.7],
-        [12,  6.6,  8.4,  5.2],
-        [13,  4.8,  6.3,  3.6],
-        [14,  4.2,  6.2,  3.4]
+        [1,  56, 80.8],
+        [2,  52, 69.5],
+        [3,  48,   57],
+        [4,  44, 18.8],
+        [5,  40, 17.6],
+        [6,  36, 13.6],
+        [7,  32, 12.3],
+        [8,  28, 29.2],
+        [9,  24, 42.9],
+        [10, 20, 30.9],
+        [11, 16,  7.9],
+        [12, 12,  8.4],
+        [13, 8,  6.3],
+        [14, 0,  6.2]
       ]);
 
       var options = {
