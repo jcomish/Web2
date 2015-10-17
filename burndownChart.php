12e2fe236b31f77d3ec69aab5467be9819042fa2
jcomish <jomish2323@gmail.com>
2015-10-17 07:44:23 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang = "en">
-  <head>
-    <title>Joshua Comish</title>
-    <link rel="stylesheet" type="text/css" href="Jomish.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8"/>
-  </head>
-
-  <header>
-    </br>
-    <h4><a href="index.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Home</a> </h4>
-    <h4><a href="assign032.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Projects</a> </h4>
-    <h4><a href="assignments.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Assignments</a></h4>
-    </br>
-    </br>
-  </header>
-<body>
-  </br>
-  </br>
 <?php
  define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
  define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
@@ -45,27 +26,96 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
       $rel[] = $row;
     }
 
-
 $statement = $db->query("SELECT * FROM milestone");
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     { 
       $milestones[] = $row;
     }
-foreach ($milestones as $value)
-{
-  echo $value['name'] . '</br>';
-}
 
   $statement = $db->query("SELECT * FROM task");
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     { 
       $task[] = $row;
     }
-foreach ($task as $value)
-{
-  echo $value['name'] . '</br>';
-}
 ?>
+
+
+<html lang = "en">
+  <head>
+    <title>Joshua Comish</title>
+    <link rel="stylesheet" type="text/css" href="Jomish.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8"/>
+
+    <!--Load the Ajax API-->
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script type="text/javascript">
+          // Load the Visualization API and the piechart package.
+    google.load('visualization', '1.1', {packages: ['line']});
+            // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'Guardians of the Galaxy');
+      data.addColumn('number', 'The Avengers');
+      data.addColumn('number', 'Transformers: Age of Extinction');
+
+      data.addRows([
+        [1,  37.8, 80.8, 41.8],
+        [2,  30.9, 69.5, 32.4],
+        [3,  25.4,   57, 25.7],
+        [4,  11.7, 18.8, 10.5],
+        [5,  11.9, 17.6, 10.4],
+        [6,   8.8, 13.6,  7.7],
+        [7,   7.6, 12.3,  9.6],
+        [8,  12.3, 29.2, 10.6],
+        [9,  16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+        [11,  5.3,  7.9,  4.7],
+        [12,  6.6,  8.4,  5.2],
+        [13,  4.8,  6.3,  3.6],
+        [14,  4.2,  6.2,  3.4]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Box Office Earnings in First Two Weeks of Opening',
+          subtitle: 'in millions of dollars (USD)'
+        },
+        width: 900,
+        height: 500
+      };
+
+      var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
+      chart.draw(data, options);
+    }
+  </script>
+
+
+
+
+
+
+  </head>
+
+  <header>
+    </br>
+    <h4><a href="index.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Home</a> </h4>
+    <h4><a href="assign032.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Projects</a> </h4>
+    <h4><a href="assignments.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Assignments</a></h4>
+    </br>
+    </br>
+  </header>
+<body>
+  </br>
+  </br>
+
+<!--this is the div that will hold the pie chart-->
+<div id="chart_div"></div>
 </body>
 
 
