8cae6971a3c0cb00e9befd0c065251c6371800a5
jcomish <jomish2323@gmail.com>
2015-10-17 00:51:10 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -16,10 +16,7 @@
         die();
       }
 
-//$con=mysql_connect("localhost","Username","Password") or die("Failed to connect with database!!!!");
-//mysql_select_db("Database Name", $con); 
-// The Chart table contains two fields: weekly_task and percentage
-// This example will display a pie chart. If you need other charts such as a Bar chart, you will need to modify the code a little to make it work with bar chart and other charts
+//Get the data
 $statement = $db->query("USE project");
 $statement = echo $sth->query("SELECT * FROM rel")
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
@@ -28,66 +25,8 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
        echo $row['name'] . '</b>:<b>';
        echo $row['due_date'] . '</b> </br></br></p2>';
     }
-
-/*
----------------------------
-example data: Table (Chart)
---------------------------
-weekly_task     percentage
-Sleep           30
-Watching Movie  40
-work            44
-*/
-
-$rows = array();
-//flag is not needed
-$flag = true;
-$table = array();
-$table['cols'] = array(
-
-    // Labels for your chart, these represent the column titles
-    // Note that one column is in "string" format and another one is in "number" format as pie chart only required "numbers" for calculating percentage and string will be used for column title
-    array('label' => 'Weekly Task', 'type' => 'string'),
-    array('label' => 'Percentage', 'type' => 'number')
-
-);
- 
 ?>
 
-<html>
-  <head>
-    <!--Load the Ajax API-->
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script type="text/javascript">
 
-    // Load the Visualization API and the piechart package.
-    google.load('visualization', '1', {'packages':['corechart']});
 
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(<?=$jsonTable?>);
-      var options = {
-           title: 'My Weekly Plan',
-          is3D: 'true',
-          width: 800,
-          height: 600
-        };
-      // Instantiate and draw our chart, passing in some options.
-      // Do not forget to check your div ID
-      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
-    </script>
-  </head>
-
-  <body>
-    <!--this is the div that will hold the pie chart-->
-    <div id="chart_div"></div>
-  </body>
-</html>
   
