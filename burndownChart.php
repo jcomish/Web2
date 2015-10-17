315687d0ced79e7457c95b1704ed400e19230eea
jcomish <jomish2323@gmail.com>
2015-10-17 18:49:30 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -58,14 +58,13 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
   <script type="text/javascript">
     // Load the Visualization API and the piechart package.
-    google.load('visualization', '1', {packages: ['line', 'corechart']});
+    google.load('visualization', '1.0', {packages: ['corechart']});
     // Set a callback to run when the Google Visualization API is loaded.
     google.setOnLoadCallback(init);
 
 
     function init () {
       drawChart1();
-      google.load('visualization', '1', {packages: ['line', 'corechart']});
       drawChart2();
     }
 
