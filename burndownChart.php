cc93730acdb948e387518e436c5129750f01baf4
jcomish <jomish2323@gmail.com>
2015-10-17 19:50:36 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -26,6 +26,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
       $rel[] = $row;
     }
 
+//Dropdown menu
 echo "<select name=\"rel\">"; 
 echo "<option size =30 ></option>";
 foreach ($rel as $value) {
@@ -48,27 +49,38 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 
 
 <html>
-      <head>
-        <!--Load the AJAX API-->
-        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-        <script type="text/javascript">
+ <head>
+  <!--Load the AJAX API-->
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript">
 
-          // Load the Visualization API and the piechart package.
-          google.load('visualization', '1.0', {'packages':['corechart']});
+  // Load the Visualization API and the piechart package.
+  google.load('visualization', '1.0', {'packages':['corechart']});
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.setOnLoadCallback(init);
 
-          // Set a callback to run when the Google Visualization API is loaded.
-          google.setOnLoadCallback(init);
-          function init()
-          {
-            drawChart1();
-            //drawChart2();
-          }
+  function init()
+  {
+    <?php
+    $js_array = json_encode($milestones);
+    echo "var js_milestones = ". $js_array . ";\n";
+    ?>
+    for (int i = 0; i < js_milestones.length(); i++)
+    {
+      for (int j = 0; j < js_milestones[i].length(); j++)
+      {
+        js_milestones[i][j];
+      }
+    }
 
-          function drawChart1() 
-          {
+    drawChart1();
+  }
 
-            // Create the data table.
-var data2 = new google.visualization.DataTable();
+    function drawChart1() 
+    {
+
+      // Create the data table.
+      var data2 = new google.visualization.DataTable();
       data2.addColumn('date', 'Time (In Hours)');
       data2.addColumn('number', 'Due Date');
       data2.addColumn('number', 'Progress');
@@ -93,7 +105,7 @@ var data2 = new google.visualization.DataTable();
 
 
             // Set chart options
-var options2 = {
+      var options2 = {
         chart: {
           title: 'Rocket Boots',
           subtitle: 'Time remaining in hours'
@@ -105,46 +117,11 @@ var options2 = {
 
 
             // Instantiate and draw our chart, passing in some options.
-            var chart2 = new google.visualization.LineChart(document.getElementById('chart_div2'));
-            chart2.draw(data2, options2);
+      var chart2 = new google.visualization.LineChart(document.getElementById('chart_div2'));
+      chart2.draw(data2, options2);
 
-          }
-          /*function drawChart2()
-          {
+    }
 
-var data3 = new google.visualization.DataTable();
-      data3.addColumn('date', 'Time (In Hours)');
-      data3.addColumn('number', 'Due Date');
-      data3.addColumn('number', 'Progress');
-
-      data3.addRows([
-        [new Date(2015, 10, 16),  56, 55],
-        [new Date(2015, 10, 17),  52, 53],
-        [new Date(2015, 10, 18),  48, 51],
-        [new Date(2015, 10, 19),  44, 49],
-        [new Date(2015, 10, 20),  40, 42],
-        [new Date(2015, 10, 21),  36, 36],
-        [new Date(2015, 10, 22),  32, 31],
-        [new Date(2015, 10, 23),  28, 25],
-        [new Date(2015, 10, 24),  24, 20],
-        [new Date(2015, 10, 25), 20, 15],
-        [new Date(2015, 10, 26), 16,  11],
-        [new Date(2015, 10, 27), 12,  6],
-        [new Date(2015, 10, 28), 8,  2],
-        [new Date(2015, 10, 29), 0,  0]
-      ]);
-
-      var options3 = {
-        chart: {
-          title: 'Rocket Boots',
-          subtitle: 'Time remaining in hours'
-        },
-        width: 400,
-        height: 300
-      };
-                  var chart3 = new google.visualization.LineChart(document.getElementById('chart_div3'));
-            chart3.draw(data3, options3);
-          }*/
           
         </script>
       </head>
