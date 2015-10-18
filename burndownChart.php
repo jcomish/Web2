4f51323624cc18a3eae20f5317734ba5dcaf223f
jcomish <jomish2323@gmail.com>
2015-10-17 22:01:11 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -1,9 +1,4 @@
 <?php
-if($_POST['submit'] && $_POST['submit'] != 0)
-{
-  echo "TEST!";
-   $selRelease = $_POST['release'];
-}
  define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
  define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
  define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
