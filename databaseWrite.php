11016a99001004cc2253078c9d8b0876fb5b0cbf
jcomish <jomish2323@gmail.com>
2015-10-21 23:39:31 -0500

    assignment 4

--- databaseWrite.php
+++ databaseWrite.php
@@ -84,7 +84,7 @@
     $statement = $db->query('SELECT * FROM topic');
     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     {
-      echo '<input type="checkbox" name="topic" value="' . $row['id'] . '">' . $row['name'] . '<br/>';
+      echo '<input type="checkbox" name="topic" value="' . $row['name'] . '">' . $row['name'] . '<br/>';
     }
     ?>
   </form>
