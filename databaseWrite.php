b5cac01e0e3371c004759c455e0a4be4fdb22579
jcomish <jomish2323@gmail.com>
2015-10-21 22:22:13 -0500

    assignment 4

--- databaseWrite.php
+++ databaseWrite.php
@@ -26,8 +26,16 @@
 
     if(isset($_POST["book"]))
     {
-      $statement = $db->exec("INSERT INTO scriptures (book, chapter, verse, content) VALUES (" . $_POST['book'] . ", " . $_POST['chapter'] . ", " . $_POST['verse'] . ", " . $_POST['content'] . ")");
-      $statement = $db->exec("INSERT INTO link (id, topic) VALUES (" . $db->lastInsertId() . ", " . $_POST['topic'] . ")");
+      $sql = "INSERT INTO scriptures (book, chapter, verse, content) VALUES (" . $_POST['book'] . ", " . $_POST['chapter'] . ", " . $_POST['verse'] . ", " . $_POST['content'] . ")";
+      if ( $fetched = $d->query($sql)){
+        $fetched->execute(PDO::FETCH_BOTH);
+
+      $sql = "INSERT INTO link (id, topic) VALUES (" . $db->lastInsertId() . ", " . $_POST['topic'] . ")";
+      if ( $fetched = $d->query($sql)){
+        $fetched->execute(PDO::FETCH_BOTH);
+
+      //$statement = $db->exec("INSERT INTO scriptures (book, chapter, verse, content) VALUES (" . $_POST['book'] . ", " . $_POST['chapter'] . ", " . $_POST['verse'] . ", " . $_POST['content'] . ")");
+      //$statement = $db->exec("INSERT INTO link (id, topic) VALUES (" . $db->lastInsertId() . ", " . $_POST['topic'] . ")");
    }
 
   ?>
