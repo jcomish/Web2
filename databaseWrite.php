ad38542b413457c7856e2ec4fe050650b43ba8b1
jcomish <jomish2323@gmail.com>
2015-10-21 22:46:37 -0500

    assignment 4

--- databaseWrite.php
+++ databaseWrite.php
@@ -30,8 +30,8 @@
     try 
     {
     //$sql = "INSERT INTO scriptures (book, chapter, verse, content) VALUES (" . $_POST['book'] . ", " . $_POST['chapter'] . ", " . $_POST['verse'] . ", " . $_POST['content'] . ")";
-    $sql = "INSERT INTO scriptures (book, chapter, verse, content) VALUES (Test1, test2, test3, test4)";
-    $sth = $db->query($sql);
+    $sql = $db->query('USE scriptures');
+    $sql = $db->query("INSERT INTO scriptures (book, chapter, verse, content) VALUES ('Test1', 'test2', 'test3', 'test4')");
     } 
     catch(PDOException $e) 
     {
