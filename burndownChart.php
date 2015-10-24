65c2692460c17e34fa6b780b51bb5d0d235f62d9
jcomish <jomish2323@gmail.com>
2015-10-23 22:47:11 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -1,11 +1,3 @@
-  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-  <script type="text/javascript">
-
-  // Load the Visualization API and the piechart package.
-  google.load('visualization', '1.0', {'packages':['corechart']});
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.setOnLoadCallback(drawChart);
-  </script>
 <?php
  define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
  define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
@@ -119,6 +111,7 @@ foreach($milestones as $value)
 }
       $toJS = json_encode($JSON);
   echo "<script>drawChart(" . $toJS . ");</script>";
+  echo "<div id='chart_div2'></div>"
 
 ?>
 
@@ -129,13 +122,13 @@ foreach($milestones as $value)
     <link rel="stylesheet" type="text/css" href="Jomish.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8"/>
   <!--Load the AJAX API-->
-
+  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
   <script type="text/javascript">
 
   // Load the Visualization API and the piechart package.
-  //google.load('visualization', '1.0', {'packages':['corechart']});
+  google.load('visualization', '1.0', {'packages':['corechart']});
   // Set a callback to run when the Google Visualization API is loaded.
-  //google.setOnLoadCallback(drawChart);
+  google.setOnLoadCallback(drawChart);
 
     function drawChart(data) 
     {
@@ -194,7 +187,7 @@ foreach($milestones as $value)
     </br>
   </header>
       <body>
-        <div id='chart_div2'></div>
+
         <!--Divs that will hold the charts-->
       </body>
     </html>
