fa12008f6bc4fe4ca315a0d7637f7a751a9744b3
jcomish <jomish2323@gmail.com>
2015-10-23 21:50:44 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -108,7 +108,7 @@ foreach($milestones as $value)
     }
     echo "</table></br><p3>Time Remaining: " . $ETC . "</p3><br/><br/></br></br>";
     $toJS = json_encode($JSON, $ETC);
-    echo "<script>drawChart(" . $JSON . ", " . $ETC . ");</script>";
+    echo "<script>drawChart(" . $toJS . ", " . $ETC . ");</script>";
     echo "<div id='chart_div2'></div>";
   }
 }
