b5c2532a81c1588207686abb47b9392ce914d150
jcomish <jomish2323@gmail.com>
2015-10-24 13:31:46 -0500

    assignment 4

--- login_success.php
+++ login_success.php
@@ -1,5 +1,3 @@
-/ Check if session is not registered, redirect back to main page.
-// Put this code in first line of web page.
 <?php
 session_start();
 if(!session_is_registered(myusername)){
--- logout.php
+++ logout.php
@@ -1,4 +1,3 @@
-// Put this code in first line of web page.
 <?php
 session_start();
 session_destroy();
