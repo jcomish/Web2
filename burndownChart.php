8d2fcd538e5593bedca1d6b8891966c3a76fc26a
jcomish <jomish2323@gmail.com>
2015-10-23 23:15:16 -0500

    assignment 4

--- burndownChart.php
+++ burndownChart.php
@@ -1,5 +1,5 @@
 <?php
- /*define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+ define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
  define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
  define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
  define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
@@ -110,87 +110,6 @@ foreach($milestones as $value)
   }
 }
       //$data = json_encode($JSON);
-*/
+
 ?>
 
-
-<html>
- <head>
-  <title>Joshua Comish</title>
-    <link rel="stylesheet" type="text/css" href="Jomish.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8"/>
-  <!--Load the AJAX API-->
-  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-  <script type="text/javascript">
-
-  // Load the Visualization API and the piechart package.
-  google.load('visualization', '1.0', {'packages':['corechart']});
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.setOnLoadCallback(drawChart);
-
-    function drawChart() 
-    {
-      //window.alert(JSON.stringify(<?php$data?>));
-
-      // Create the data table.
-      var data2 = new google.visualization.DataTable();
-      data2.addColumn('date', 'Time (In Hours)');
-      data2.addColumn('number', 'Due Date');
-      data2.addColumn('number', 'Progress');
-
-      data2.addRows([
-        [new Date(2015, 10, 16),  56, 55],
-        [new Date(2015, 10, 17),  52, 53],
-        [new Date(2015, 10, 18),  48, 51],
-        [new Date(2015, 10, 19),  44, 49],
-        [new Date(2015, 10, 20),  40, 42],
-        [new Date(2015, 10, 21),  36, 36],
-        [new Date(2015, 10, 22),  32, 31],
-        [new Date(2015, 10, 23),  28, 25],
-        [new Date(2015, 10, 24),  24, 20],
-        [new Date(2015, 10, 25), 20, 15],
-        [new Date(2015, 10, 26), 16,  11],
-        [new Date(2015, 10, 27), 12,  6],
-        [new Date(2015, 10, 28), 8,  2],
-        [new Date(2015, 10, 29), 0,  0]
-      ]);
-
-            // Set chart options
-      var options2 = {
-        chart: {
-          title: 'Rocket Boots',
-          subtitle: 'Time remaining in hours'
-        },
-        width: 900,
-        height: 700
-      };
-            // Set chart options
-
-
-            // Instantiate and draw our chart, passing in some options.
-      var chart2 = new google.visualization.LineChart(document.getElementById('chart_div2'));
-      chart2.draw(data2, options2);
-
-    }
-
-          
-        </script>
-      </head>
-<header>
-    </br>
-    <h4><a href="index.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Home</a> </h4>
-    <h4><a href="assign032.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Projects</a> </h4>
-    <h4><a href="assignments.html" onMouseOver="this.style.color='White'" onMouseOut="this.style.color='Orange'">Assignments</a></h4>
-    </br>
-    </br>
-  </header>
-      <body>
-        <div id='chart_div2'></div>
-        <!--Divs that will hold the charts-->
-      </body>
-    </html>
-
-
-
-
-  
