883d01f6f91cc6b91566fc74ca4e95d835f86f73
jcomish <jomish2323@gmail.com>
2015-10-25 17:20:36 -0500

    assignment 4

--- model.php
+++ model.php
@@ -85,7 +85,7 @@ function add_member($username, $password) {
         echo 'Error!: ' . $ex->getMessage();
         die();
       }
-    	$password = password_hash("'" $password "'", PASSWORD_DEFAULT);
+    	$password = password_hash($password, PASSWORD_DEFAULT);
         echo $password;
         $query = $db->query("USE members");
         $query = $db->query("INSERT INTO members (username, password) VALUES ('$username', '$password')");
